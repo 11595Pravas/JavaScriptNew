4ea923acea6c829a5b092ced5de058daa3548dcf
Pravas Mohanty <141177667+11595Pravas@users.noreply.github.com>
2024-01-30 16:19:08 +0000

    datatype

---
+++ .devcontainer/01.Basic/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+alert(3+3) //we are using nodejs,not browser so it shows error
+
+console.log(3+3)
+
+let name="pravas"
+console.log("name");
+
+//number => 2 to power 53
+//bigint
+//string=> ""
+// boolean=>true/false
+// null =>standalone value
+// undefined=>
+// symbol => unique
+
+// object
+
+
+console.log();
