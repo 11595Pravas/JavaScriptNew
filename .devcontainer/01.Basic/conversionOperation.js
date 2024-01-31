a8fecc70162d193d31344f685929c7b154eb8174
Pravas Mohanty <141177667+11595Pravas@users.noreply.github.com>
2024-01-31 20:56:20 +0000

    changes

--- .devcontainer/01.Basic/conversionOperation.js
+++ .devcontainer/01.Basic/conversionOperation.js
@@ -1,60 +1,64 @@
-let score="123as"
-let a= null
-let c=undefined
-let e=false
-let g=1
+// let score="123as"
+// let a= null
+// let c=undefined
+// let e=false
+// let g=1
 
-//NaN =>not a number
+// //NaN =>not a number
 
-console.log(score);
-console.log(typeof score);
- let updatescore=Number(score)
- console.log(updatescore)
- console.log(typeof updatescore);
-console.log(typeof(updatescore));
-
-
-console.log("-----------------------------------")
+// console.log(score);
+// console.log(typeof score);
+//  let updatescore=Number(score)
+//  console.log(updatescore)
+//  console.log(typeof updatescore);
+// console.log(typeof(updatescore));
 
 
-console.log(a);
-console.log(typeof(a));
-let b=Number(a)
-console.log(b);
-console.log(typeof b);
+// console.log("-----------------------------------")
 
 
-console.log("-----------------------------------")
+// console.log(a);
+// console.log(typeof(a));
+// let b=Number(a)
+// console.log(b);
+// console.log(typeof b);
 
 
-console.log(c);
-console.log(typeof(c));
-let d=Number(c)
-console.log(d);
-console.log(typeof d);
+// console.log("-----------------------------------")
 
 
-// node .devcontainer/01.Basic/conversionOperation.js
+// console.log(c);
+// console.log(typeof(c));
+// let d=Number(c)
+// console.log(d);
+// console.log(typeof d);
 
 
-console.log("-----------------------------------")
+// // node .devcontainer/01.Basic/conversionOperation.js
 
 
-console.log(e);
-console.log(typeof(e));
-let f=String(e)
-console.log(f);
-console.log(typeof f);
-
-console.log("-----------------------------------")
+// console.log("-----------------------------------")
 
 
-console.log(g);
-console.log(typeof(g));
-let h=Boolean(g)
-console.log(h);
-console.log(typeof h);
+// console.log(e);
+// console.log(typeof(e));
+// let f=String(e)
+// console.log(f);
+// console.log(typeof f);
+
+// console.log("-----------------------------------")
+
+
+// console.log(g);
+// console.log(typeof(g));
+// let h=Boolean(g)
+// console.log(h);
+// console.log(typeof h);
 
 //1 =>true ; 0=>false
 //"" => false
 //"pravas" => true
+
+let a=5
+console.log(a);
+console.log(typeof a);
