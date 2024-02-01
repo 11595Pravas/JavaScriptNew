034e2a05d0f529ad40b698936505a0455dc040ac
Pravas Mohanty <141177667+11595Pravas@users.noreply.github.com>
2024-02-01 10:34:47 +0000

    updated

--- .devcontainer/01.Basic/datatype.js
+++ .devcontainer/01.Basic/datatype.js
@@ -1,4 +1,4 @@
-"use strict"; //treat all js code as newer version
+/*"use strict"; //treat all js code as newer version
 
 //alert(3+3) //we are using nodejs,not browser so it shows error
 
@@ -19,3 +19,59 @@ console.log("name");
 
 
 console.log();
+
+*/
+
+
+
+
+
+console.log("------------------------------------------");
+// Note
+
+/*
+primitive datatype 7 types:String,Number,Boolean,null,undefined,symbol,BigInt
+
+Reference(Non primitive):Array,object,Functions
+
+
+*/
+
+//primitive
+let a="pravas"
+let b=34
+let c=23.54
+let d=true
+let insideTemo=null
+let outsideTemp;
+
+const id=Symbol('123')
+const anotherId=Symbol('123as')
+
+console.log(typeof id);
+
+console.log(id == anotherId);
+console.log(id === anotherId);
+
+const bigNumber =55555556798763456n
+console.log(typeof bigNumber);
+console.log(bigNumber);
+
+//Reference(Non primitive):Array,Function
+const heros=["saktiman","spiderman","superman"];
+let myobj={
+    name:"pravas",
+    age:22,
+    Home:"Balesore"
+}
+
+console.log(heros);
+console.log(typeof heros);
+
+console.log(myobj);
+console.log(typeof myobj);
+
+const myfunction = function (params) {
+    console.log("hello world");
+}
+console.log(typeof myfunction);
