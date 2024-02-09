65f7bad3ab904a7019363ca644580ca9163b16a4
Pravas Mohanty <141177667+11595Pravas@users.noreply.github.com>
2024-02-09 15:45:32 +0000

    math and number

---
+++ .devcontainer/01.Basic/nums_and_math.js
@@ -0,0 +1,34 @@
+const score=233
+// console.log(score);
+// console.log(typeof score);
+
+const balance=new Number (1232)
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(7));
+
+const othernumber=123.678
+// console.log(othernumber.toPrecision(3));
+
+const another=1000000000000
+// console.log(another.toLocaleString('en-IN'));
+
+//++++++++++++Math+++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.009));
+// console.log(Math.ceil(4.0002));
+// console.log(Math.floor(5.006));
+// console.log(Math.min(2,4,6,1,0,6,9));
+// console.log(Math.max(5,3,2,1,7,89,56,33));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
