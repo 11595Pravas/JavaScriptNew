6699cae8a3e22fb098d86672de5ef4da45293e3f
Pravas Mohanty <141177667+11595Pravas@users.noreply.github.com>
2024-02-12 12:44:59 +0000

    IIFE

--- .devcontainer/03.Basic/IIFE.js
+++ .devcontainer/03.Basic/IIFE.js
@@ -1,6 +1,14 @@
 // Immediately Invoked Function Expressions(IIFE)
 (function chai() {
     console.log(`db Connected`);
-})()
+})();
 
 //To avoid global scope pollution we use IIFE (function declaration)(function calling)
+
+( () => {
+    console.log(`DB Connected`);
+})();
+
+( (n1,n2) => {
+    console.log(n1+n2);
+})(2,3);
